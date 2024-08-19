2e5db848f40003862da64babe14beee96db783ca
benhensor <benhensor@gmail.com>
2024-08-19 15:54:50 +0100

    config update

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Test database connection
 sequelize.authenticate()
