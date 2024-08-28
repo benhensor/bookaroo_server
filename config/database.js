046af220174ffc6323ffb91b8b6885841cf03600
benhensor <benhensor@gmail.com>
2024-08-28 16:04:46 +0100

    auth fix

--- config/database.js
+++ config/database.js
@@ -4,12 +4,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-console.log('Initializing Sequelize with the following configuration:', {
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-});
+// console.log('Initializing Sequelize with the following configuration:', {
+//   database: process.env.DB_NAME,
+//   username: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+// });
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
