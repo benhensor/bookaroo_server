d3f65d725786bc4fb8ad6cfc5cbc196d47e3fe68
benhensor <benhensor@gmail.com>
2024-09-04 12:16:29 +0100

    auth fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -116,7 +116,7 @@ export const login = async (req, res) => {
 
 		res.cookie('authToken', token, {
 			httpOnly: true,
-			sameSite: 'none',
+			sameSite: 'strict',
 			secure: true,
 			maxAge: 24 * 60 * 60 * 1000,
 		})
